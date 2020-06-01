652a0ae88497e38cfa0389e22e757c49c96f2777
fbry-alx <fabry.pappacena@gmail.com>
2020-06-01 09:59:07 -0700

    finished habit routes

--- controllers/habit.js
+++ controllers/habit.js
@@ -5,13 +5,39 @@ const db = require('../models');
 
 /* Index */
 router.get('/', (req, res) => {
-  res.send('/habit')
+  res.send('/habit');
+});
+
+/* New */
+router.get('/new', (req, res) => {
+  res.send('/habit/new');
+});
+
+/* Show */
+router.get('/:id', (req, res) => {
+  res.send('/habit/:id');
 });
 
 
+/* Create */
+router.post('/', (req, res) => {
+  res.send('/habit');
+});
 
+/* Edit */
+router.get('/:id/edit', (req, res) => {
+  res.send('/habit/:id/edit');
+});
 
+/* Update */
+router.put('/:id', (req, res) => {
+  res.send('/habit/:id');
+});
 
+/* Delete */
+router.delete('/:id', (req, res) => {
+  res.send('/habit');
+});
 
 
 
