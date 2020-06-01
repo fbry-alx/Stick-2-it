c6cfb87a22d48e8569c2e779843a4df9a0ccd078
fbry-alx <fabry.pappacena@gmail.com>
2020-06-01 10:01:36 -0700

    set up activity routes

--- controllers/activity.js
+++ controllers/activity.js
@@ -5,10 +5,39 @@ const db = require('../models');
 
 /* Index */
 router.get('/', (req, res) => {
-  res.send('/activity')
+  res.send('/activity');
+});
+
+/* New */
+router.get('/new', (req, res) => {
+  res.send('/activity/new');
+});
+
+/* Show */
+router.get('/:id', (req, res) => {
+  res.send('/activity/:id');
 });
 
 
+/* Create */
+router.post('/', (req, res) => {
+  res.send('/activity');
+});
+
+/* Edit */
+router.get('/:id/edit', (req, res) => {
+  res.send('/activity/:id/edit');
+});
+
+/* Update */
+router.put('/:id', (req, res) => {
+  res.send('/activity/:id');
+});
+
+/* Delete */
+router.delete('/:id', (req, res) => {
+  res.send('/activity');
+});
 
 
 
