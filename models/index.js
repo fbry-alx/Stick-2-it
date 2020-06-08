6bac629f8bb5d8039bdbc6b624ca64ad3c0ac19a
Zach <ziggi24@gmail.com>
2020-06-08 11:50:07 -0700

    for the heroku gods

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.MONGODB_URI || "mongodb://localhost:27017/habit";
+const connectionString = process.env.MONGOLAB_URI || "mongodb://localhost:27017/habit";
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
